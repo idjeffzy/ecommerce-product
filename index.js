32b5af505b16e500e44692d316d59980f6942b4e
idjeffzy <idjeffzy@gmail.com>
2021-12-13 14:23:28 +0100

    header& carousel

---
+++ index.js
@@ -0,0 +1,13 @@
+function opensidenav(){
+  document.querySelector(".sidenav").style.width = "68%";
+  document.querySelector(".sidenav").style.left = "0";
+
+}
+document.querySelector(".sidenavbtn").addEventListener("click", opensidenav);
+
+function closesidenav(){
+  document.querySelector(".sidenav").style.width = "0";
+  document.querySelector(".sidenav").style.left = "-50px";
+}
+
+document.querySelector(".closebtn").addEventListener("click", closesidenav);
