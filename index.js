733a86242cd42d3e92a3522d84c6c409341ed547
idjeffzy <idjeffzy@gmail.com>
2021-12-14 16:52:32 +0100

    caro-test

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function closesidenav(){
 }
 
 document.querySelector(".closebtn").addEventListener("click", closesidenav);
+
+// Carousel Funcionality
+
+// function nextImg() {
+//   var count = 1;
+//   var currentImg = "images\image-product-"+ count +".jpg";
+//   document.querySelector("caro-img").setAttribute("src", currentImg);
+//
+//   count++;
+// }
+
+function changeImg() {
+  document.querySelector(".caro-img").setAttribute("src", "images/image-product-2.jpg");
+}
+
+document.querySelector(".caro-nxt").addEventListener("click", changeImg);
