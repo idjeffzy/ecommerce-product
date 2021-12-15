20b3f7a8093196278ff77f47654343f489886ac3
idjeffzy <idjeffzy@gmail.com>
2021-12-15 22:42:20 +0100

    pricing test

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ function closesidenav(){
 document.querySelector(".closebtn").addEventListener("click", closesidenav);
 
 // Carousel Funcionality
+
+var count = 1
+
+document.querySelector(".caro-nxt").addEventListener("click", function () {
+  count++;
+  if (count > 4) {
+    count = 4;
+  }
+  var currentImg = "images/image-product-"+ count +".jpg";
+  document.querySelector(".caro-img").setAttribute("src", currentImg);
+
+});
+
+document.querySelector(".caro-prev").addEventListener("click", function () {
+  count--;
+  if (count<1) {
+    count=1;
+  }
+  var currentImg = "images/image-product-"+ count +".jpg";
+  document.querySelector(".caro-img").setAttribute("src", currentImg);
+});
+// END
+
+// Pricing Calc
+var count = 1;
+let items = document.querySelector(".no-of-itms").innerHTML;
+document.querySelector(".add-itm").addEventListener("click", function () {
+  items++;
+  items = count;
+})
