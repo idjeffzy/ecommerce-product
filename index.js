e9e8efd2d7dcf27cbc0ce987393279a58c3e700c
idjeffzy <idjeffzy@gmail.com>
2021-12-16 20:09:59 +0100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function opensidenav(){
   document.querySelector(".sidenav").style.width = "68%";
   document.querySelector(".sidenav").style.left = "0";
-
+  document.querySelector("body").classList.toggle("cutted");
 }
 document.querySelector(".sidenavbtn").addEventListener("click", opensidenav);
 
 function closesidenav(){
   document.querySelector(".sidenav").style.width = "0";
   document.querySelector(".sidenav").style.left = "-50px";
+  document.querySelector("body").classList.toggle("cutted");
 }
 
 document.querySelector(".closebtn").addEventListener("click", closesidenav);
